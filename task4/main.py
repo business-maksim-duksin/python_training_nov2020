228dd7e307cf3cb66c2cd2fbada80fd9adcfefd6
maksim <business.maxtheduksin@gmail.com>
2020-11-29 19:34:53 +0300

    Using Task1 as a starting point of Task4.

---
+++ task4/main.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import json
+from save_as import save_as_json, save_as_xml
+from logging_config import log
+
+
+parser = argparse.ArgumentParser(description="Form a list of rooms containing list of students inside every room.")
+parser.add_argument("students", type=str, help="path to students json")
+parser.add_argument("rooms", type=str, help="path to rooms json")
+parser.add_argument("output_format", type=str, help="output file format")
+parser.add_argument("-o", "--output_path", type=str, help="output file path", default=str(os.getcwd()))
+parser.add_argument("-n", "--output_name", type=str, help="output file name", default="result")
+args = parser.parse_args()
+
+savers = {
+    "json": save_as_json,
+    "xml": save_as_xml,
+}
+# Check if valid save format
+try:
+    save_to_file = savers[args.output_format]
+except KeyError:
+    msg = f"Unknown file format {args.output_format}, use one of these: {', '.join(savers.keys())}."
+    log.error(msg)
+    raise
+
+def main():
+    with open(args.students, 'r') as f:
+        students = json.load(f)
+    with open(args.rooms, 'r') as f:
+        rooms = json.load(f)
+
+    [room.update({"Students": []}) for room in rooms]
+    rooms_and_students = {"Rooms":
+                              {room["id"]:
+                                   room for room in rooms
+                               }
+                          }
+    [rooms_and_students["Rooms"][student["room"]]["Students"].append(student) for student in students]
+    # Get rid of useless depth layer  (room id as keys)
+    rooms_and_students["Rooms"] = list(rooms_and_students["Rooms"].values())
+
+    filename = f"{args.output_name}.{args.output_format}"
+    save_to_file(rooms_and_students, os.path.join(args.output_path, filename))
+
+
+if __name__ == "__main__":
+    main()
---
+++ task4/save_as.py
@@ -0,0 +1,47 @@
+import json
+import xml.etree.ElementTree as ET
+from typing import Dict
+from logging_config import log
+
+
+def save_as_json(d: Dict, path: str) -> None:
+    with open(path, 'w') as f:
+        json.dump(d, f, indent=4)
+
+
+def save_as_xml(d: Dict, path: str) -> None:
+    root_key = list(d.keys())[0]
+    root = ET.Element(str(root_key))
+
+    def get_leafs(stuff):
+        """ Recursively walk data branches forming a XML ElementTree. """
+        leafs = []
+        if isinstance(stuff, dict):
+            for key, value in stuff.items():
+                elem = ET.Element(str(key))
+                if isinstance(value, dict):
+                    elem.extend(get_leafs(value))
+                elif isinstance(value, list):
+                    elem.extend(get_leafs(value))
+                else:
+                    elem.text = str(value)
+                leafs.append(elem)
+        elif isinstance(stuff, list):
+            for value in stuff:
+                elem = ET.Element("obj")
+                if isinstance(value, dict):
+                    elem.extend(get_leafs(value))
+                elif isinstance(value, list):
+                    for node in value:
+                        elem.extend(get_leafs(node))
+                else:
+                    elem.text = str(value)
+                leafs.append(elem)
+        return leafs
+
+    for item in d[root_key]:
+        root.extend(get_leafs(item))
+
+    tree = ET.ElementTree(root)
+    with open(path, "w") as f:
+        tree.write(f, encoding="unicode")
